ed97e96b1b1f6f39caea61d53d35a5f218df3518
Shivani <guptashivani021@gmail.com>
2025-12-22 23:47:05 -0800

    troubleshooting

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
+  base: '/', // Change to '/gayatri/' if your repo name is 'gayatri' and you're not using a custom domain
 })
 
